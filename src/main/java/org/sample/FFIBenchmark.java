1731318afec70cde5c71c9a432b41100686a3576
Yuki Misumi <u7582380@anu.edu.au>
2024-03-09 15:40:55 +1100

    adjusted to java 21

--- src/main/java/org/sample/FFIBenchmark.java
+++ src/main/java/org/sample/FFIBenchmark.java
@@ -1,34 +1,38 @@
 package org.sample;
 
 
-import jdk.incubator.foreign.CLinker;
-import jdk.incubator.foreign.FunctionDescriptor;
-import jdk.incubator.foreign.NativeSymbol;
-import jdk.incubator.foreign.ValueLayout;
 import org.openjdk.jmh.annotations.*;
 import org.openjdk.jmh.runner.Runner;
 import org.openjdk.jmh.runner.RunnerException;
 import org.openjdk.jmh.runner.options.Options;
 import org.openjdk.jmh.runner.options.OptionsBuilder;
 
+import java.lang.foreign.*;
 import java.lang.invoke.MethodHandle;
 import java.util.concurrent.TimeUnit;
 
-@Fork(warmups = 2, value = 2, jvmArgs = {"-Xms512m", "-Xmx1024m", "--enable-native-access=ALL-UNNAMED", "--add-modules", "jdk.incubator.foreign"})
+@Fork(warmups = 2, value = 2, jvmArgs = {"-Xms512m", "-Xmx1024m", "--enable-native-access=ALL-UNNAMED", "--enable-preview"})
 @BenchmarkMode(value = Mode.AverageTime)
 @OutputTimeUnit(TimeUnit.NANOSECONDS)
 @Warmup(iterations = 5, time = 500, timeUnit = TimeUnit.MILLISECONDS)
 @Measurement(iterations = 20, time = 500, timeUnit = TimeUnit.MILLISECONDS)
 @State(Scope.Thread)
 public class FFIBenchmark {
+    // Java 21 API
+    Linker linker = Linker.nativeLinker();
+    SymbolLookup libc = linker.defaultLookup();
+    MemorySegment getpidAddr = libc.find("getpid").get();
+    FunctionDescriptor getpidSig = FunctionDescriptor.of(ValueLayout.JAVA_INT);
+    MethodHandle getpid = linker.downcallHandle(getpidAddr, getpidSig);
 
+    // Java 19 API
     // get System linker
-    private static final CLinker linker = CLinker.systemCLinker();
-    // predefine symbols and method handle info
-    private static final NativeSymbol nativeSymbol = linker.lookup("getpid").get();
-    private static final MethodHandle getPidMH = linker.downcallHandle(
-            nativeSymbol,
-            FunctionDescriptor.of(ValueLayout.OfInt.JAVA_INT));
+//    private static final CLinker linker = CLinker.systemCLinker();
+//    // predefine symbols and method handle info
+//    private static final NativeSymbol nativeSymbol = linker.lookup("getpid").get();
+//    private static final MethodHandle getPidMH = linker.downcallHandle(
+//            nativeSymbol,
+//            FunctionDescriptor.of(ValueLayout.OfInt.JAVA_INT));
 
     public static void main(String[] args) throws RunnerException {
         Options opt = new OptionsBuilder()
@@ -40,10 +44,10 @@ public class FFIBenchmark {
     }
 
 // uncomment this when running on macOS
-// @Benchmark
-// public int JNI() {
-//     return org.bytedeco.javacpp.macosx.getpid();
-// }
+//    @Benchmark
+//    public int JNI() {
+//        return org.bytedeco.javacpp.macosx.getpid();
+//    }
 
     @Benchmark
     public int JNI() {
@@ -52,11 +56,11 @@ public class FFIBenchmark {
 
     @Benchmark
     public int panamaDowncall() throws Throwable {
-        return (int) getPidMH.invokeExact();
+        return (int) getpid.invokeExact();
     }
 
-    @Benchmark
-    public int panamaJExtract() {
-        return org.unix.unistd_h.getpid();
-    }
+//    @Benchmark
+//    public int panamaJExtract() {
+//        return org.unix.unistd_h.getpid();
+//    }
 }
